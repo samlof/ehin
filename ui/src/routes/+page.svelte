89a839b4b948d2329abb51035c1ed054d8efc0d2
Samuli Löf <samuli.lof@gmail.com>
2025-03-26 13:18:09 +0200

    fixes 2

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -7,8 +7,7 @@
 	const timeNow = new Date();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Sähkö hinnat</h1>
 <div>
 	{#each data.prices as p}
 		<div>
