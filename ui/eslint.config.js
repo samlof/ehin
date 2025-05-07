8e0ca22e54f3235d2091015cd051c218c2c5c1a4
Samuli Löf <samuli.lof@gmail.com>
2025-05-07 13:13:39 +0300

    update front packages

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -19,9 +19,9 @@ export default ts.config(
 		languageOptions: {
 			globals: {
 				...globals.browser,
-				...globals.node
-			}
-		}
+				...globals.node,
+			},
+		},
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
@@ -32,8 +32,8 @@ export default ts.config(
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	}
+				svelteConfig,
+			},
+		},
+	},
 );
